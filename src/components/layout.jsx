d15f98917f3cc4253c8631c36077d557d11da5b5
Zahoor Ahmed <82461526+ZahoorKaladi@users.noreply.github.com>
2025-10-20 00:37:31 +0500

    Rename Navbar and Footer components to lowercase

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,11 +6,11 @@ import Footer from './footer'; // <-- Import the new Footer component
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      <navbar />
       <main className="flex-grow">
         {children}
       </main>
-      <Footer /> {/* <-- Add the Footer here */}
+      <footer /> {/* <-- Add the Footer here */}
     </div>
   );
 };
